c3a946d4e94e6c973057d73d2030cba02659a153
shrey deshwal <shreyashdeshwal@gmail.com>
2021-08-05 01:02:10 +0530

    primer plus ch8 excercise

---
+++ ch8-q1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define EOF -1
+int main()
+{
+    int count = 0;
+    char ch;
+    
+    printf("enter the line:");
+    while ((ch=getchar())!=EOF)
+    {
+    
+       count++;
+        
+        
+    }
+    printf("no. of words in the line are %d",count);
+    
+
+    
+     return 0;
+     
+     
+     
+}
